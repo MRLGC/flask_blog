a1ea1e493ec26f495be7748ac53f91e400380088
unknown <984684973@qq.com>
2018-05-03 00:49:21 +0800

    修改config

--- app.py
+++ app.py
@@ -5,30 +5,36 @@ from view_fun.admin import admin_blueprint as admin
 from models import db
 from view_fun.admin import login_opt
 from view_fun.admin import ckeditor
-app = Flask(__name__)
-app.config.from_object('config')
-Bootstrap(app)
-db.init_app(app)
-ckeditor.init_app(app)
-login_opt.init_app(app)
-app.register_blueprint(index)
-app.register_blueprint(admin)
-
-@app.before_request
-def create_data():
-	db.create_all()
-
-if not app.debug:
-	import logging
-	from logging.handlers import RotatingFileHandler
-	fileHandler = RotatingFileHandler('logfile/Myblog.log', mode='a', maxBytes=1*1024*1024, backupCount=10)
-	formatStr = '%(asctime)s %(levelname)s: %(message)s [%(pathname)s:%(lineno)d]'
-	logFormate = logging.Formatter(formatStr)
-	fileHandler.setFormatter(logFormate)
-	app.logger.setLevel(logging.INFO)
-	fileHandler.setLevel(logging.INFO)
-	app.logger.addHandler(fileHandler)
-	app.logger.info('Myblog startup')
-
+from config import DevConfig, ProdConfig
+import platform
+def createApp():
+	app = Flask(__name__)
+	sysstr = platform.system()
+	if sysstr == 'Linux':
+		app.config.from_object(ProdConfig)
+	else:
+		app.config.from_object(DevConfig)
+	Bootstrap(app)
+	db.init_app(app)
+	ckeditor.init_app(app)
+	login_opt.init_app(app)
+	app.register_blueprint(index)
+	app.register_blueprint(admin)
+	@app.before_request
+	def create_data():
+		db.create_all()
+	if not app.debug:
+		import logging
+		from logging.handlers import RotatingFileHandler
+		fileHandler = RotatingFileHandler('logfile/Myblog.log', mode='a', maxBytes=1*1024*1024, backupCount=10)
+		formatStr = '%(asctime)s %(levelname)s: %(message)s [%(pathname)s:%(lineno)d]'
+		logFormate = logging.Formatter(formatStr)
+		fileHandler.setFormatter(logFormate)
+		app.logger.setLevel(logging.INFO)
+		fileHandler.setLevel(logging.INFO)
+		app.logger.addHandler(fileHandler)
+		app.logger.info('Myblog startup')
+	return app
 if __name__ == '__main__':
+	app = createApp()
 	app.run(port=5300,debug=True,host='0.0.0.0')
--- config.py
+++ config.py
@@ -1,4 +1,12 @@
-SECRET_KEY = 'you never get'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-CKEDITOR_FILE_UPLOADER = 'admin.upload'
+import os
+class Config(object):
+    SECRET_KEY = 'you never get'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CKEDITOR_FILE_UPLOADER = 'admin.upload'
+ 
+class ProdConfig(Config):
+    env = os.environ
+    SQLALCHEMY_DATABASE_URI = env.get('MYSQLCONNECTOR')
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///blog.db'
